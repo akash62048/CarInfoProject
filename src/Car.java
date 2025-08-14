3905b526e04e32f0824ec1c42d5f7e90454408fd
Akash <akashpoddar492@gmail.com>
2025-08-14 12:14:33 +0530

    Initial commit: set up CarInfoProject with Car class

---
+++ src/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+    public static void main(String[] args) {
+        String company = "Toyota";
+        String model = "Camry";
+        double mileage = 30.58;
+
+        System.out.println("my car company name is "+ company
+                           +" ,and the model is "+ model +
+                           " ,that gives " + mileage +" mileage ");
+
+    }
+}
